e38a68c3384cf026ae9b93d4a978530692017546
zhu <blacknstones@gmail.com>
2021-01-01 23:10:45 +0100

    added User model

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true;
+        minlength: 3
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+export { User }
+
