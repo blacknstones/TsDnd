e5f171e25f244bf892f495f7aa6c18ff123ac268
zhu <blacknstones@gmail.com>
2021-01-09 23:37:41 +0100

    clean up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import './App.css';
-import Board from './components/Board';
+import * as React from 'react';
+import { Board } from './components/Board';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Board/>
