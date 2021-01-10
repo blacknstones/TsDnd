f0aa1c6375ebfea8f66a2173a10dc3c7350d2106
zhu <blacknstones@gmail.com>
2021-01-10 21:47:45 +0100

    rename server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,18 @@
-import {config} from 'dotenv';
+import { config } from 'dotenv';
 import express from 'express';
 import { connect } from './database';
 import  cors from 'cors';
 
 config();
 
-const app = express();
+const server = express();
 const port = process.env.PORT ||  5000;
 
-app.use(cors());
-app.use(express.json());
+server.use(cors());
+server.use(express.json());
 
 connect();
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is running on ${port}`);
 })
