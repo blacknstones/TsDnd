15669b7211a1105d4f562b601b6784c5d899e8fd
Yanwei Zhu <47001967+blacknstones@users.noreply.github.com>
2021-08-29 23:58:54 +0200

    frontend api cleanup

--- frontend/src/api/Api.ts
+++ frontend/src/api/Api.ts
@@ -1,14 +1,21 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosInstance, AxiosResponse } from "axios";
 
 const BASE_URL: string = "http://localhost:4000";
 
 // create axios instance 
 const Api: AxiosInstance = axios.create({
     baseURL: BASE_URL,
+    timeout: 15000,
 });
 
 Api.interceptors.request.use((config) => {
     
 })
 
-export default Api;
+const handleResponse = (response: AxiosResponse) => response.data;
+
+const requests = {
+    get: (url: string) => 
+}
+
+export default Api; 
--- frontend/src/services/list.ts
+++
@@ -1,3 +0,0 @@
-// an experienmental attempt to use RTK query 
-
-import {createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
